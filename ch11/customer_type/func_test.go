d4e8106ccd934960b10d88efa6be8f86bf1c905a
KidoThunder <zgdisgod@hotmail.com>
2021-05-14 11:03:27 +0800

    update: customer type test

--- ch11/customer_type/func_test.go
+++ ch11/customer_type/func_test.go
@@ -26,3 +26,21 @@ func TestFn(t *testing.T) {
 	tsSF := timeSpent(slowFn)
 	t.Log(tsSF(10))
 }
+
+func TestXx(t *testing.T){
+	s1 := []int{1, 2, 3, 4}
+	s2 := s1[1:2]
+	fmt.Println(s2)
+	s2 = append(s2, 5)
+	fmt.Println(s2)
+	for _, v := range s2 {
+		v += 10
+	}
+	fmt.Println(s2)
+	fmt.Println(s1)
+	for i := range s2 {
+		s2[i] += 10
+	}
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
